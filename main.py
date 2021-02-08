d6121e3dd5948c67c4d3a575d3773b24f8a8c08b
Chiranjeev Mishra <54413679+chirrumishra@users.noreply.github.com>
2021-02-08 11:30:27 +0530

    beatification changes

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ for i in range(distance):
         start -= load_capacity
 #Here if condition is checking that camel doesn't move back if there is only one banana left.
         if start == 1:
-            lose=lose-1#Lose is decreased because if camel try to get remaining one banana he will lose one extra banana for covering that two miles.
+            lose -= 1
+#Lose is decreased because if camel try to get remaining one banana he will lose one extra banana for covering that two miles.
 #Here we are increasing lose because for moving backward and forward by one mile two bananas will be lose
         lose += 2
 #Here lose is decreased as in last trip camel will not go back.
